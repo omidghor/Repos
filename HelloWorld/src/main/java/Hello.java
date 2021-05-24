3abc65bcca0c573e1efcd3fbbba2f291ff5fbb7e
“omidghor” <omid.ghorbany@gmail.com>
2021-05-24 11:31:53 -0700

    Hello World in NetBeans

---
+++ HelloWorld/src/main/java/Hello.java
@@ -0,0 +1,15 @@
+/**
+ *
+ * @author omidghor
+ *email: omid.ghorbany@gmail.com
+ *date: 05/24/2021
+ *purpose: My first Hello World program in an IDE
+ */
+
+package com.omidghor.helloworld;
+
+public class Hello {
+  public static void main(String[] args) {
+        System.out.println("Hello, World!");
+  }
+}
