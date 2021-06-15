0dbdf546f3363c87589a732ccde33e0774d887a2
“omidghor” <omid.ghorbany@gmail.com>
2021-06-14 21:15:57 -0700

    assessment

---
+++ sg.foundations.flowcontrol.arrays/src/main/java/com/sg/foundations/flowcontrol/arrays/FruitsBasket.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.foundations.flowcontrol.arrays;
+
+/**
+ *
+ * @author omidghor
+ */
+public class FruitsBasket {
+    public static void main(String[] args) {
+        String[] fruitBasket = {"Orange", "Apple", "Orange", "Apple", "Orange", "Apple",
+            "Orange", "Apple", "Orange", "Orange", "Orange", "Apple", "Orange", "Orange",
+            "Apple", "Orange", "Orange", "Apple", "Apple", "Orange", "Apple", "Apple",
+            "Orange", "Orange", "Apple", "Apple", "Apple", "Banana", "Apple", "Orange",
+            "Orange", "Apple", "Apple", "Orange", "Orange", "Orange", "Orange", "Apple",
+            "Apple", "Apple", "Apple", "Orange", "Orange", "PawPaw", "Apple", "Orange",
+            "Orange", "Apple", "Orange", "Orange", "Apple", "Apple", "Orange", "Orange",
+            "Apple", "Orange", "Apple", "Kiwi", "Orange", "Apple", "Orange",
+            "Dragonfruit", "Apple", "Orange", "Orange"};
+
+        int numOranges = 0;
+        int numApples = 0;
+        int numOtherFruit = 0;
+
+        // Fruit counting code goes here!
+
+        // Print The Results!
+
+    }
+}
---
+++ sg.foundations.flowcontrol.arrays/src/main/java/com/sg/foundations/flowcontrol/arrays/StillPositive.java
@@ -0,0 +1,18 @@
+
+package com.sg.foundations.flowcontrol.arrays;
+
+/**
+ *
+ * @author omidghor
+ */
+public class StillPositive {
+    public static void main(String[] args) {
+        int[] numbers = { 389, -447, 26, -485, 712, -884, 94, -64, 868, -776, 227, -744, 422, -109, 259, -500, 278, -219, 799, -311};
+        
+        for (int num : numbers) {
+            if (num > 0) {
+                System.out.println(num);
+            }
+        }
+    }
+}
